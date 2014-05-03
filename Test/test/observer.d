0e989cbe7e90bed88ae17a31e82d0c8e035d78df
AndreSteenveld <andre.steenveld@gmail.com>
2014-05-03 12:05:30 +0200

    Adding more unit tests for the Observer and Notifier

--- Test/test/observer.d
+++ Test/test/observer.d
@@ -5,10 +5,21 @@ version( unittest ){
 	import std.stdio;	
 	import std.exception;
 
+	import core.exception;
+
 	import rdx.Observer;
 	import rdx.Notification;
 	import rdx.NotificationKind;
 	
+	//
+	// Observer!* . toObserver tests
+	//
+	unittest {
+
+		std.exception.assertThrown!AssertError( Observer!int.toObserver( null ) );
+
+	}
+
 	unittest { 
 		
 		int i = 0;
@@ -27,6 +38,77 @@ version( unittest ){
 		
 	}
 
+	unittest {
+
+		Throwable throwable = new Throwable( "Test exception" );
+
+		int i = 0;
+
+		void next ( Notification!int n ) {
+			assert( i++ == 0 );
+			assert( n.kind == NotificationKind.ON_ERROR );
+			assert( n.error.opEquals( throwable ) );
+			assert( !n.hasValue );
+		}
+
+		auto observer = Observer!int.toObserver( &next );
+
+		observer.onError( throwable );
+
+	}
+
+	//
+	// Observer!* . ToNotifier tests
+	//
+
+	class TestObserver : Observer!int {
 	
+		public int hasOnNext;
+		public Throwable hasOnError;
+		public bool hasOnComplete;
+
+		override  void next( int value ) { this.hasOnNext = value; }
+	
+		override  void error( Throwable error ) { this.hasOnError = error; }
+		
+		override  void complete( ) { this.hasOnComplete = true; }
+		
+	}
+
+	unittest {
+
+		std.exception.assertThrown!AssertError( Observer!int.toNotifier( null ) );
+	
+	}
+	
+	unittest {
+
+		auto o = new TestObserver( );
+		o.toNotifier( )( Notification!int.createOnNext( 42 ) );
+
+		assert( o.hasOnNext == 42 );
+
+	}
+
+	unittest {
+		
+		Throwable throwable = new Throwable( "Our test throwable" );
+
+		auto o = new TestObserver( );
+		o.toNotifier( )( Notification!int.createOnError( throwable ) );
+
+		assert( throwable.opEquals( o.hasOnError ) );
+
+	}
+	
+	unittest {
+
+		auto o = new TestObserver( );
+		o.toNotifier( )( Notification!int.createOnCompleted( ) );
+
+		assert( o.hasOnComplete );
+
+	}
+
 }
 
